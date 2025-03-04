5cc971692aadec5aba274aff73472d87390e1ea6
NotTheMark <145426266+NotTheMark@users.noreply.github.com>
2025-03-04 12:17:49 +0100

    dolog

--- WPF/Hun/obj/Debug/net7.0-windows/Terkep.g.i.cs
+++ WPF/Hun/obj/Debug/net7.0-windows/Terkep.g.i.cs
@@ -55,13 +55,13 @@ namespace RPG_nagymarci_WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RPG_nagymarci_WPF;component/terkep.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RPG_nagymarci_WPF;V1.0.0.0;component/terkep.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Terkep.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +71,7 @@ namespace RPG_nagymarci_WPF {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
